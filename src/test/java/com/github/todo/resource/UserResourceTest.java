6ef4ae5df4e2cadee8785e3d05e491dc62f500d1
Lanlyhs <ziqiang.chen@outlook.com>
2022-04-30 23:07:47 +0800

    Update remove unless import

--- src/test/java/com/github/todo/resource/UserResourceTest.java
+++ src/test/java/com/github/todo/resource/UserResourceTest.java
@@ -1,16 +1,12 @@
 package com.github.todo.resource;
 
 import com.github.todo.DBRollbackBaseTest;
-import com.github.todo.domain.user.User;
 import com.github.todo.domain.user.UserRepository;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
